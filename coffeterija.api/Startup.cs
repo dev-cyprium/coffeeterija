4f1ad71c18c13a736b883bd15b6ab34d33720bf7
Stefan Kupresak <stefan_vg@hotmail.com>
2019-06-13 02:45:16 +0200

    Add static files to the project

--- coffeterija.api/Startup.cs
+++ coffeterija.api/Startup.cs
@@ -38,6 +38,8 @@ namespace coffeterija.api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseStaticFiles();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
